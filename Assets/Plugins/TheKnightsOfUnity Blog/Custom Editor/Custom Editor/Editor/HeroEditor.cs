22a7d54a88fc4b0adb9787dbe3f2a65a70db91b1
ExtremeZ7 <d0ntd1e@yahoo.com>
2017-03-07 19:14:32 +0800

    Learned Property Drawers and Custom Editors

---
+++ Assets/Plugins/TheKnightsOfUnity Blog/Custom Editor/Custom Editor/Editor/HeroEditor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor (typeof(Hero))]
+public class HeroEditor : Editor
+{
+	SerializedProperty health;
+	SerializedProperty maxHealth;
+	SerializedProperty withShield;
+	SerializedProperty shield;
+
+	void OnEnable ()
+	{
+		health = serializedObject.FindProperty ("health");
+		maxHealth = serializedObject.FindProperty ("maxHealth");
+		withShield = serializedObject.FindProperty ("withShield");
+		shield = serializedObject.FindProperty ("shield");
+	}
+
+	public override void OnInspectorGUI ()
+	{
+		serializedObject.UpdateIfDirtyOrScript ();
+
+		EditorGUILayout.IntSlider (health, 0, maxHealth.intValue, "Health");
+
+		EditorGUI.indentLevel++;
+		EditorGUILayout.PropertyField (maxHealth, new GUIContent ("Max Health"));
+		EditorGUI.indentLevel--;
+
+		EditorGUILayout.Space ();
+
+		EditorGUILayout.PropertyField (withShield, new GUIContent ("With Shield"));
+
+		if (withShield.boolValue) {
+			EditorGUI.indentLevel++;
+			EditorGUILayout.PropertyField (shield, new GUIContent ("Shield"));
+			EditorGUI.indentLevel--;
+		}
+
+		serializedObject.ApplyModifiedProperties ();
+	}
+}
---
+++ Assets/Plugins/TheKnightsOfUnity Blog/Custom Editor/Custom Editor/Hero.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+	public int health;
+	public int maxHealth = 100;
+	public bool withShield;
+	public int shield;
+}
