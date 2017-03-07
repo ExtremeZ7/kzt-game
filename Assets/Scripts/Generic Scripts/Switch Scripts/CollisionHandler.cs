c47fe9af968057bee88dc256a8b31f15dcdacda3
ExtremeZ7 <d0ntd1e@yahoo.com>
2017-03-07 12:11:33 +0800

    Added MyPropertyDrawers.cs

--- Assets/Scripts/Generic Scripts/Switch Scripts/CollisionHandler.cs
+++ Assets/Scripts/Generic Scripts/Switch Scripts/CollisionHandler.cs
@@ -6,223 +6,201 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using AssemblyCSharp;
 
 public class CollisionHandler : MonoBehaviour
 {
-    //enums
-    public enum ActivationMode
-    {
-        Default,
-        OnlyActivateOnce,
-        FlipActivation}
-    ;
+	//enums
+	public enum ActivationMode
+	{
+		Default,
+		OnlyActivateOnce,
+		FlipActivation}
+	;
 
-    //fields
-    [Tooltip("This is an optional attribute used for easier seeking from Listeners")]
-    public string switchName;
+	//fields
+	[Tooltip ("This is an optional attribute used for easier seeking from Listeners")]
+	public string switchName;
 
-    [Space(10)]
-    [Tooltip("Defualt: Normal activation on trigger\n"
-        + "Only Activate Once: Prevents from being triggered more than once\n"
-        + "Flip Activation: Flip active bit when triggered")]
-    public ActivationMode activationMode;
+	[Space (10)]
+	[Tooltip ("Defualt: Normal activation on trigger\n"
+	+ "Only Activate Once: Prevents from being triggered more than once\n"
+	+ "Flip Activation: Flip active bit when triggered")]
+	public ActivationMode activationMode;
 
-    [Space(10)]
-    public List<Tags> tagFilter;
+	[Space (10)]
+	public List<Tags> tagFilter;
 
-    [Space(10)]
-    [Tooltip("When enabled, activation will last only for one frame!")]
-    public bool deactivateAfterOneFrame;
-    [Tooltip("If set, the trigger will only activate if and only if other "
-        + "triggers in the scene that also have exclusive activation have not "
-        + "been activated on the same frame. Triggers with this bit not set "
-        + "will not be affected. (This is mainly used in the crate triggers so "
-        + "that only one crate is destroyed per frame)")]
-    public bool exclusiveActivation;
-    [Tooltip("This enables the 'OnStay2D' trigger. Disabled by default.")]
-    public bool activateOnStay;
-    [Tooltip("This enables the 'OnExit2D' trigger. Enabled by default.")]
-    public bool deactivateOnExit = true;
+	[Space (10)]
+	[Tooltip ("When enabled, activation will last only for one frame!")]
+	public bool deactivateAfterOneFrame;
+	[Tooltip ("If set, the trigger will only activate if and only if other "
+	+ "triggers in the scene that also have exclusive activation have not "
+	+ "been activated on the same frame. Triggers with this bit not set "
+	+ "will not be affected. (This is mainly used in the crate triggers so "
+	+ "that only one crate is destroyed per frame)")]
+	public bool exclusiveActivation;
+	[Tooltip ("This enables the 'OnStay2D' trigger. Disabled by default.")]
+	public bool activateOnStay;
+	[Tooltip ("This enables the 'OnExit2D' trigger. Enabled by default.")]
+	public bool deactivateOnExit = true;
 
-    private bool triggeredOnce;
-    private GameObject triggerObj;
-    private bool active;
-    private bool activatedOnCurrentFrame;
-    private bool deactivatedOnCurrentFrame;
+	bool triggeredOnce;
+	GameObject triggerObj;
+	bool active;
+	bool activatedOnCurrentFrame;
+	bool deactivatedOnCurrentFrame;
 
-    //static fields
-    private static bool exclusiveSwitch;
+	//static fields
+	static bool exclusiveSwitch;
 
-    //properties
-    public GameObject TriggeringObject
-    {
-        get{ return triggerObj; }
-    }
+	//properties
+	public GameObject TriggeringObject {
+		get{ return triggerObj; }
+	}
 
-    protected List<string> TagFilter
-    {
-        get{ return tagFilter.ToStringList(); }
-    }
+	protected List<string> TagFilter {
+		get{ return tagFilter.ToStringList (); }
+	}
 
-    public bool ActivatedOnCurrentFrame
-    {
-        get{ return activatedOnCurrentFrame; }
-    }
+	public bool ActivatedOnCurrentFrame {
+		get{ return activatedOnCurrentFrame; }
+	}
 
-    public bool DeactivatedOnCurrentFrame
-    {
-        get{ return deactivatedOnCurrentFrame; }
-    }
+	public bool DeactivatedOnCurrentFrame {
+		get{ return deactivatedOnCurrentFrame; }
+	}
 
-    public bool IsActivated
-    {
-        get{ return active || activatedOnCurrentFrame; }
-    }
+	public bool IsActivated {
+		get{ return active || activatedOnCurrentFrame; }
+	}
 
-    //methods
-    /// <summary>Called by a derived class whenever its trigger is called</summary>
-    /// <param name=”triggerObj”>The object that caused the trigger</param>
-    ///
-    protected void Trigger(GameObject triggerObj)
-    {
-        //Return if the object tag is not in the filter
-        if (!TagFilter.Contains(triggerObj.tag))
-        {
-            return;
-        }
+	//methods
+	/// <summary>
+	/// Called by a derived class whenever its trigger is called
+	/// </summary>
+	/// <param name="Obj">The object that caused the trigger</param>
+	///
+	protected void Trigger (GameObject Obj)
+	{
+		//Return if the object tag is not in the filter
+		if (!TagFilter.Contains (Obj.tag)) {
+			return;
+		}
 
-        //Return if exclusive activation and exclusive bit has been set
-        if (exclusiveActivation && exclusiveSwitch)
-        {
-            return;
-        }
+		//Return if exclusive activation and exclusive bit has been set
+		if (exclusiveActivation && exclusiveSwitch) {
+			return;
+		}
 
-        this.triggerObj = triggerObj;
-        switch (activationMode)
-        {
-            case ActivationMode.Default:
-                {
-                    SetActiveState(true);
-                    if (exclusiveActivation)
-                    {
-                        exclusiveSwitch = true;
-                    }
-                }
-                break;
+		triggerObj = Obj;
+		switch (activationMode) {
+		case ActivationMode.Default:
+			{
+				SetActiveState ();
+				exclusiveSwitch |= exclusiveActivation;
+			}
+			break;
 
-            case ActivationMode.OnlyActivateOnce:
-                {
-                    if (triggeredOnce)
-                    {
-                        break;
-                    }
-                    triggeredOnce = true;
-                    goto case (int)ActivationMode.Default;
-                }
+		case ActivationMode.OnlyActivateOnce:
+			{
+				if (triggeredOnce) {
+					break;
+				}
+				triggeredOnce = true;
+				goto case (int)ActivationMode.Default;
+			}
 
-            case ActivationMode.FlipActivation:
-                SetActiveState(!active);
-                break;
-        }
-    }
+		case ActivationMode.FlipActivation:
+			SetActiveState (!active);
+			break;
+		}
+	}
 
-    /// <summary>Usually called by a derived class whenever its trigger is exited</summary>
-    /// <param name=”triggerObj”>The object that caused the trigger</param>
-    ///
-    protected void ExitTrigger(GameObject triggerObj)
-    {
-        //Return if this is not the same object that caused the latest trigger
-        if (triggerObj != this.triggerObj)
-        {
-            return;
-        }
+	/// <summary>
+	/// Usually called by a derived class whenever its trigger is exited
+	/// </summary>
+	/// <param name="Obj">The object that caused the trigger</param>
+	///
+	protected void ExitTrigger (GameObject Obj)
+	{
+		//Return if this is not the same object that caused the latest trigger
+		if (Obj != triggerObj) {
+			return;
+		}
 
-        //Return if the 'deactivateOnExit' bit is not set
-        if (!deactivateOnExit)
-        {
-            return;
-        }
+		//Return if the 'deactivateOnExit' bit is not set
+		if (!deactivateOnExit) {
+			return;
+		}
 
-        //Return if exclusive activation and exclusive bit has been set
-        if (exclusiveActivation && exclusiveSwitch)
-        {
-            return;
-        }
+		//Return if exclusive activation and exclusive bit has been set
+		if (exclusiveActivation && exclusiveSwitch) {
+			return;
+		}
 
-        switch (activationMode)
-        {
-            case ActivationMode.Default:
-            case ActivationMode.OnlyActivateOnce:
-                SetActiveState(false);
-                break;
-        }
-        this.triggerObj = null;
-    }
+		switch (activationMode) {
+		case ActivationMode.Default:
+		case ActivationMode.OnlyActivateOnce:
+			SetActiveState (false);
+			break;
+		}
+		triggerObj = null;
+	}
 
-    /// <summary>Sets the active bit then starts other operations</summary>
-    /// <param name=”active”>Whether or not the active bit should be set</param>
-    ///
-    protected void SetActiveState(bool active = true)
-    {
-        //Start the below coroutine only if the active bit was flipped
-        if (this.active != active)
-        {
-            StartCoroutine(SetSingleFrameBits(active));
-        }
+	/// <summary>Sets the active bit then starts other operations</summary>
+	/// <param name="active">Whether or not the active bit should be set</param>
+	///
+	protected void SetActiveState (bool active = true)
+	{
+		//Start the below coroutine only if the active bit was flipped
+		if (this.active != active) {
+			StartCoroutine (SetSingleFrameBits (active));
+		}
 
-        this.active = active;
+		this.active = active;
 
-        //Start the below coroutine only if the 'deactivateAfterOneFrame' bit is
-        //set and the active bit is set
-        if (deactivateAfterOneFrame && this.active)
-        {
-            StartCoroutine(DeactivateWhenFrameEnds());
-        }
-    }
+		//Start the below coroutine only if the 'deactivateAfterOneFrame' bit is
+		//set and the active bit is set
+		if (deactivateAfterOneFrame && this.active) {
+			StartCoroutine (DeactivateWhenFrameEnds ());
+		}
+	}
 
-    /// <summary>A coroutine that sets some bits to true then waits for the end of the frame before resetting them</summary>
-    /// <param name=”active”>Tells which bit to set</param>
-    ///
-    private IEnumerator SetSingleFrameBits(bool active = true)
-    {
-        if (active)
-        {
-            activatedOnCurrentFrame = true;
-        }
-        else
-        {
-            deactivatedOnCurrentFrame = true;
-        }
+	/// <summary>A coroutine that sets some bits to true then waits for the end of the frame before resetting them</summary>
+	/// <param name="active">Tells which bit to set</param>
+	///
+	IEnumerator SetSingleFrameBits (bool active = true)
+	{
+		if (active) {
+			activatedOnCurrentFrame = true;
+		} else {
+			deactivatedOnCurrentFrame = true;
+		}
 
-        yield return new WaitForEndOfFrame();
+		yield return new WaitForEndOfFrame ();
 
-        if (active)
-        {
-            activatedOnCurrentFrame = false;
-        }
-        else
-        {
-            deactivatedOnCurrentFrame = false;
-        }
+		if (active) {
+			activatedOnCurrentFrame = false;
+		} else {
+			deactivatedOnCurrentFrame = false;
+		}
 
-        //Reset the exclusive bit so that the next frame can use it again
-        exclusiveSwitch = false;
-    }
+		//Reset the exclusive bit so that the next frame can use it again
+		exclusiveSwitch = false;
+	}
 
-    /// <summary>A coroutine that resets the active bit at the end of a frame</summary>
-    ///
-    private IEnumerator DeactivateWhenFrameEnds()
-    {
-        yield return new WaitForEndOfFrame();
-        active = false;
-    }
+	/// <summary>A coroutine that resets the active bit at the end of a frame</summary>
+	///
+	IEnumerator DeactivateWhenFrameEnds ()
+	{
+		yield return new WaitForEndOfFrame ();
+		active = false;
+	}
 
-    void OnValidate()
-    {
-        if (switchName == "")
-        {
-            switchName = gameObject.name + this.GetHashCode();
-        }
-    }
+	void OnValidate ()
+	{
+		if (switchName == "") {
+			switchName = gameObject.name + GetHashCode ();
+		}
+	}
 }
---
+++ Assets/Scripts/Project Mark III/Sandbox/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	[Range (10f, 80f)]
+	public float attackPower = 10f;
+
+	//#if UNITY_EDITOR
+	//[MyPropertyDrawers.PopuAttribute ("Hello, "Howdy", "DIE!!", "How can I help you?", "Let's dance!")]
+	//#endif
+	public string defaultGreeting;
+
+	//#if UNITY_EDITOR
+	//[MyPropertyDrawers.IncrementalChange(5f)]
+	//#endif
+	public float lookAngle = 0f;
+}
